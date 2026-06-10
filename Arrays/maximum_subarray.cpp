fefb29d1cb575e73782ee6f9bf8714b0a9ef6d7f
divanshi12 <164319060+divanshi12@users.noreply.github.com>
2026-06-10 14:17:26 +0530

    Create maximum_subarray.cpp

---
+++ Arrays/maximum_subarray.cpp
@@ -0,0 +1,22 @@
+// Problem : Maximum Subarray
+// Approach : Kadane's Algorithm
+// Time Compexity : O(n)
+// Space Complexity : O(1)
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currSum = 0, maxSum = INT_MIN;
+
+        for(int val : nums) {
+            currSum += val;
+            maxSum = max(currSum, maxSum);
+
+            if (currSum < 0) {
+                currSum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+};
